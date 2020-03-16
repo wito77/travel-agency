2e3b1d59e408a58d561f351d8a81f5dd7f1eda7b
Kacper Witosław <wito77pl@gmail.com>
2020-03-16 21:33:33 +0100

    Add function for sending orders to api

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -4,6 +4,40 @@ import { Grid, Row, Col } from 'react-flexbox-grid';
 import OrderSummary from '../OrderSummary/OrderSummary';
 import pricing from '../../../data/pricing.json';
 import OrderOption from '../OrderOption/OrderOption';
+import Button from '../../common/Button/Button';
+import { formatPrice } from '../../../utils/formatPrice';
+import { calculateTotal } from '../../../utils/calculateTotal';
+import settings from '../../../data/setings';
+
+const sendOrder = (options, tripCost, tripName, countryCode, tripId) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+
+  const payload = {
+    ...options,
+    totalCost,
+    tripName,
+    countryCode,
+    tripId,
+  };
+
+  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+  const fetchOptions = {
+    cache: 'no-cache',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  };
+
+  fetch(url, fetchOptions)
+    .then(function (response) {
+      return response.json();
+    }).then(function (parsedResponse) {
+      console.log('parsedResponse', parsedResponse);
+    });
+};
 
 class OrderForm extends React.Component {
 
@@ -12,10 +46,14 @@ class OrderForm extends React.Component {
     options: PropTypes.object,
     pricing: PropTypes.array,
     setOrderOption: PropTypes.func,
+    address: PropTypes.string,
+    tripName: PropTypes.string,
+    countryCode: PropTypes.node,
+    tripId: PropTypes.string,
   }
 
   render() {
-    const { tripCost, options, setOrderOption } = this.props;
+    const { tripCost, options, setOrderOption, address, tripName, tripId, countryCode } = this.props;
     console.log(pricing, options);
     return (
       <Grid>
@@ -27,6 +65,7 @@ class OrderForm extends React.Component {
           )}
           <Col xs={12}>
             <OrderSummary tripCost={tripCost} options={options} />
+            <Button onClick={() => sendOrder(options, tripCost, address, tripName, tripId, countryCode)}>Order now!</Button>
           </Col>
         </Row>
       </Grid>
--- src/components/views/Trip/Trip.js
+++ src/components/views/Trip/Trip.js
@@ -15,7 +15,7 @@ import styles from './Trip.scss';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import OrderForm from '../../features/OrderForm/OrderFormContainer';
 
-const Trip = ({ error, name, image, cost, days, description, country, intro }) => {
+const Trip = ({ error, name, image, cost, days, description, country, intro, id }) => {
   if (error) return <NotFound />;
   else return (
     <Section>
@@ -71,7 +71,7 @@ const Trip = ({ error, name, image, cost, days, description, country, intro }) =
         <Row>
           <Col xs={12}>
             <PageTitle text='Trip options' />
-            <OrderForm tripCost={cost} />
+            <OrderForm tripCost={cost} tripId={id} tripName={name} countryCode={country.alpha3Code} />
           </Col>
         </Row>
       </Grid>
@@ -86,6 +86,7 @@ Trip.propTypes = {
   days: PropTypes.number,
   description: PropTypes.string,
   country: PropTypes.object,
+  id: PropTypes.string,
 };
 
 export default Trip;
